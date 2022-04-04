41149f94a0b24a24b09ed217b7564df2b6a20872
= <danishfaisal.wwe@gmail.com>
2022-04-04 14:58:25 +0530

    10.Group Anagrams

---
+++ 10_GroupAnagrams.py
@@ -0,0 +1,14 @@
+# https://leetcode.com/problems/group-anagrams/
+
+class Solution:
+    def groupAnagrams(self, strs: List[str]) -> List[List[str]]:
+        res=defaultdict(list)
+        
+        for s in strs:
+            count=[0 for _ in range(26)]
+            for c in s:
+                count[ord(c)-ord("a")]+=1
+            
+            res[tuple(count)].append(s)
+        
+        return res.values()
