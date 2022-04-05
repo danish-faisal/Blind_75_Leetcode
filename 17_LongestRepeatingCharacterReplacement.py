1f5c2f776b468e76449892fd52faab5f2c27d76d
= <danishfaisal.wwe@gmail.com>
2022-04-05 22:46:50 +0530

    17.Longest Repeating Character Replacement

--- 17_LongestRepeatingCharacterReplacement.py
+++ 17_LongestRepeatingCharacterReplacement.py
@@ -1,5 +1,6 @@
 # https://leetcode.com/problems/longest-repeating-character-replacement/
 
+# TC - O(26*N)
 class Solution:
     def characterReplacement(self, s: str, k: int) -> int:
         res=0
@@ -10,6 +11,27 @@ class Solution:
             count[s[r]]=1+count.get(s[r],0)
             
             while (r-l+1)-max(count.values())>k:
+                count[s[l]]-=1
+                l+=1
+            
+            res=max(res,r-l+1)
+        
+        return res
+
+# TC - O(N)
+class Solution:
+    def characterReplacement(self, s: str, k: int) -> int:
+        res=0
+        count={}
+        l=0
+        
+        maxf=0
+        
+        for r in range(len(s)):
+            count[s[r]]=1+count.get(s[r],0)
+            maxf=max(maxf, count[s[r]])
+            
+            while (r-l+1)-maxf>k:
                 count[s[l]]-=1
                 l+=1
             
